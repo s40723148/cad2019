82947dce79b19086271fe300731201926d6831e1
s40723148 <40723148@gm.nfu.edu.tw>
2019-11-08 18:40:50 +0800

    11083

--- publishconf.py
+++ publishconf.py
@@ -30,8 +30,8 @@ DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-#DISQUS_SITENAME = "kmolab-github"
-#DISQUS_DISPLAY_COUNTS = False
+DISQUS_SITENAME = "kmolab-github"
+DISQUS_DISPLAY_COUNTS = False
 #GOOGLE_ANALYTICS = ""
 
 # 設定網誌以 md 檔案建立的 file system date 為準, 無需自行設定
